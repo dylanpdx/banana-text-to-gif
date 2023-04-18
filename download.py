63bbc5f4b2c35a09a08674149573e58d6ae6d34f
Dylan <dylanpdx@gmail.com>
2023-04-18 22:15:59 +0100

    Fixed model build

--- download.py
+++ download.py
@@ -4,11 +4,13 @@
 import pathlib
 from modelscope.pipelines import pipeline
 from huggingface_hub import snapshot_download
+import transformers
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     model_dir = pathlib.Path('weights')
     snapshot_download('damo-vilab/modelscope-damo-text-to-video-synthesis', repo_type='model', local_dir=model_dir)
+    transformers.utils.move_cache()
 
 if __name__ == "__main__":
     download_model()
